16bee57b66a47bdc029cf5670c146b1dab50ba3e
Rohan <sabalerohan73@gmail.com>
2021-05-18 19:08:35 +0530

    commit

--- JS/basic.js
+++ JS/basic.js
@@ -9,4 +9,55 @@
 //     difference between js and scmascript?
 //     ecma  - specification
 //     js - programming language 
-        
+
+
+//Variables - we use variable to store data temporarily
+
+//let name ;      // variables  that we define are undefined by default 
+//in javascipt
+
+// let name  = "rohan";
+// console.log(name);
+
+// rules for naming variables.
+//cannot be reserved keyword 
+// should be meaningful
+// cannot start with a number(1name)
+// cannot contain a space or hyphen (-)
+// they are case sensitive
+// declare each variable in separate line (best practice)
+
+// constant - values cannot be changed 
+//const interestRate = 0.2
+// if you dont want to reassign const should be default choice
+
+// primitive .value types
+//1. string
+//2. Number
+//3. Boolean
+//4 . undefinded
+// 5 . null
+
+// let name  = "Rohan";    // sting literal
+// let age = 21;               // number literal
+// let isAproved  = false;     // boolean literal
+// let firstName  ;
+// let lastName  = null;           // here we want to explicitly clear the value
+
+
+// reference types
+// 1. object 
+// 2.array
+// 3.function
+
+
+// Dynamic typing
+// static - declare valriable , cannot be changed 
+// dynamic - declared variable can be chaged in run TimeRanges
+// js is dynamic language   (string to number like)
+
+// typeof  - gives the datatype
+// we dont have integer and float , we have only integer
+
+// objects 
+                    
