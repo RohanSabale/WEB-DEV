620f2451f0c945a3127aba64a4b2b0843ca012b8
Rohan <sabalerohan73@gmail.com>
2021-05-18 18:44:36 +0530

    commit

---
+++ JS/basic.js
@@ -0,0 +1,12 @@
+// where does jaava script runs?
+// browser have java script engine ,
+//     firefox : spidermonkey
+//     chrome : V8
+
+//     in 2009 took opensource engine in chrome and embedded in cpp program 
+//     that called node , by this we can run js outside the browser now.
+
+//     difference between js and scmascript?
+//     ecma  - specification
+//     js - programming language 
+        
