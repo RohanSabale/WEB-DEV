990d38af99c0176e94c7961b7ff235d8a5874dac
Rohan <sabalerohan73@gmail.com>
2021-05-24 13:35:38 +0530

    commit

--- JS/NodeJS/npm.js
+++ JS/NodeJS/npm.js
@@ -1,3 +1,15 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
 
-console.log(chalk.red('Hello world!'));
+// console.log(chalk.red('Hello world!'));
+
+
+var figlet = require('figlet');
+ 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
