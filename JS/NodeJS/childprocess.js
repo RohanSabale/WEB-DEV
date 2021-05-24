fb255a5c0b3e03e1edf3165fc1dba86d2dd448b6
Rohan <sabalerohan73@gmail.com>
2021-05-24 12:05:03 +0530

    commit

---
+++ JS/NodeJS/childprocess.js
@@ -0,0 +1,9 @@
+let cp = require("child_process");
+console.log("trying to open calculator");
+
+//cp.execSync("calc");
+//cp.execSync("code");        // vs code
+//cp.execSync("start chrome https:\\ www.youtube.com");
+
+
+console.log("opened calculator");
