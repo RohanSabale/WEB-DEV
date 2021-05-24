623487e46f28e568bc412f4649ccea1c45b0ee38
Rohan <sabalerohan73@gmail.com>
2021-05-24 12:52:40 +0530

    commit

--- JS/NodeJS/fileSystem.js
+++ JS/NodeJS/fileSystem.js
@@ -1,3 +1,36 @@
+// fileSystem
 let fs = require("fs");
-let buffer  = fs.readFileSync("abc.js")
-console.log
+
+// files->create ,read ,update , delete
+
+//read
+//let buffer  = fs.readFileSync("abc.js");
+//console.log("bin data "+buffer);
+
+// create
+//fs.openSync("abc.txt","w");
+
+// no file-> create and file exist ->content replace
+//fs.writeFileSync("abc.txt","hello world");  // 
+
+//update
+//fs.appendFileSync("abc.txt","hello javascript");
+
+
+// folder
+//create
+//fs.mkdirSync("my directory");
+
+//fs.writeFileSync("my directory/mine.txt", "namaste");
+
+// let content = fs.readdirSync("my directory");
+// console.log(content);
+// for(let i = 0;i<content.length;i++)
+// {
+//     console.log("file" , content[i] ,"is removed");
+//     fs.unlinkSync("my directory/" + content[i]);
+// }
+
+// remove folder
+// fs.rmdirSync("my directory");
+
