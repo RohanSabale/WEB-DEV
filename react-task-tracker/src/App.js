e2859149ce35f58546f43e0f6e83a1269d745b2b
Rohan <sabalerohan73@gmail.com>
2021-05-27 10:22:45 +0530

    commit

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+function App() {
+  // let name = 'Rohan'
+  // const x= false
+  return (
+    <div className = 'container'>
+     <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <Header>
+            <h1>Task Tracker</h1>
+            
+        </Header>
+    )
+}
+
+export default Header
