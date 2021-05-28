c57e5ca612b8487d450808154947ead124787b4c
Rohan <sabalerohan73@gmail.com>
2021-05-28 19:30:17 +0530

    commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,12 +1,22 @@
+//import React from 'react'
 import Header from './components/Header'
-function App() {
-  // let name = 'Rohan'
-  // const x= false
+
+
+function  App() { 
+
   return (
     <div className = 'container'>
-     <Header />
+      <Header />
     </div>
   );
 }
+
+// class App extends React.Component
+// {
+//   render()
+//   {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,10 @@
+import React from 'react'
+
 const Header = () => {
     return (
-        <Header>
+        <header>
             <h1>Task Tracker</h1>
-            
-        </Header>
+        </header>
     )
 }
 
