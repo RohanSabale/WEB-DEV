4e194beb65172a6a484670d712148215d1949dfd
Rohan <sabalerohan73@gmail.com>
2021-08-06 20:25:00 +0530

    commit

---
+++ del.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        set<int>s;
+        for(int i =0;i<4;i++)
+        {
+            int k;
+            cin>>k;
+            s.insert(k);
+        }
+        if(s.size() == 1)
+            cout<<""
+    }
+}
